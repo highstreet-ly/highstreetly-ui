653bd71f44b61748384cf09928a230ba585e9ab5
Chris Chrisostomou <dev@mou.me>
2021-07-14 23:35:02 +0100

    various fixes and launch prep

--- app/pods/components/site-nav/component.js
+++ app/pods/components/site-nav/component.js
@@ -1,3 +1,7 @@
 import Component from '@glimmer/component';
 
-export default class SiteNavComponent extends Component {}
+export default class SiteNavComponent extends Component {
+  get light() {
+    return this.args.light || false;
+  }
+}
