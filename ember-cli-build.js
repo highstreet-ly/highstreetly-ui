7ef72be38acd79fa186649b0aeb95205ecbac61f
Chris Chrisostomou <dev@mou.me>
2021-07-15 11:48:52 +0100

    updated tailwind config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,35 +1,24 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
-const isProduction = EmberApp.env() === 'production';
-
-const purgeCSS = {
-  module: require('@fullhuman/postcss-purgecss'),
-  options: {
-    content: [
-      // add extra paths here for components/controllers which include tailwind classes
-      './app/index.html',
-      './app/templates/**/*.hbs',
-      './app/components/**/*.hbs',
-    ],
-    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-  },
-};
+const autoprefixer = require('autoprefixer');
+const tailwind = require('tailwindcss');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
     postcssOptions: {
       compile: {
         plugins: [
           {
-            module: require('postcss-import'),
+            module: autoprefixer,
+            options: {},
+          },
+          {
+            module: tailwind,
             options: {
-              path: ['node_modules'],
+              config: './config/tailwind.js',
             },
           },
-          require('tailwindcss')('./config/tailwind.js'),
-          ...(isProduction ? [purgeCSS] : []),
         ],
       },
     },
