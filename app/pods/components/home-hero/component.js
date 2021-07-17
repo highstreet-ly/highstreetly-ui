cb04a2297976b93cb3fd3835f5619974970c5324
Chris Chrisostomou <dev@mou.me>
2021-07-17 23:37:19 +0100

    image resizes; mobile layout; autotype only runs on tablet and above

--- app/pods/components/home-hero/component.js
+++ app/pods/components/home-hero/component.js
@@ -5,16 +5,22 @@ import Typed from 'typed.js';
 export default class HomeHeroComponent extends Component {
   typed = null;
 
+  typingTriggerWidth = 640;
+
   @action
   startTyping() {
-    var options = {
-      strings: ['quickest', 'powerful', 'cost effective', 'easiest'],
-      typeSpeed: 60,
-    };
-    this.typed = new Typed('#typing', options);
+    if (window.innerWidth >= this.typingTriggerWidth) {
+      var options = {
+        strings: ['quickest', 'powerful', 'cost effective', 'easiest'],
+        typeSpeed: 60,
+      };
+      this.typed = new Typed('#typing', options);
+    }
   }
 
   stopTyping() {
-    this.typed.destroy();
+    if (window.innerWidth >= this.typingTriggerWidth) {
+      this.typed.destroy();
+    }
   }
 }
--- config/tailwind.js
+++ config/tailwind.js
@@ -190,7 +190,13 @@ module.exports = {
     },
     container: {
       center: true,
-      padding: '2rem',
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '2rem',
+        xl: '2rem',
+        '2xl': '2rem',
+      },
     },
     content: {
       none: 'none',
